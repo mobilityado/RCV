d264b49dfcd36d3e4780841347c592940cba04bf
mobilityado <jose.lopez.vencedor@gmail.com>
2026-09-06 22:18:11 -0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// REPORT.IA RCV v51.3 · Firebase Web configurado
+// REPORT.IA RCV v51.4 · Firebase Web config verificado
 globalThis.REPORTIA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyChVxE23biZ_WqffDf9WKuxaeqwv1K79Po",
+  apiKey: "AIzaSyChVxE23biZ_WqffDf9WKuxaeqwvlK79Po",
   authDomain: "reportia-rcv.firebaseapp.com",
   projectId: "reportia-rcv",
   storageBucket: "reportia-rcv.firebasestorage.app",
   messagingSenderId: "552905070880",
-  appId: "1:552905070880:web:9c8fb780f986ad78751f45"
+  appId: "1:552905070880:web:9c8fb780f986ad78751f45",
+  measurementId: "G-2EGCMSWX11"
 };
-
-// Firebase Console > Configuración del proyecto > Cloud Messaging > Certificados push web
 globalThis.REPORTIA_VAPID_KEY = "BKV3ljvargjKoxFKe2nDV8uOVLdt5d-CXr1ioWQgr8360xl3alE2I2RxaESCTM2JZvT1-uwefwR2ioIGjFyLJn4";
